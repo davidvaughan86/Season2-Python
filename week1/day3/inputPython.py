6c827827ee712395f743c11747c6023e6f33c2ae
davidvaughan86 <david.vaughan86@gmail.com>
2021-03-10 12:52:20 -0500

    inputs and f strings

---
+++ week1/day3/inputPython.py
@@ -0,0 +1,7 @@
+#inputs and interpolation
+
+
+nameOfUser = input("what is your name?")
+print(nameOfUser)
+# we interpolate by using %s and then adding % variable(s) after the string
+print('hey %s, welcome to python' % nameOfUser)
--- week1/day3/notes.py
+++ week1/day3/notes.py
@@ -1,28 +1,30 @@
-##variables
+# ##variables
 
-string === '', 'word', 1, 'functionName'
-integer === whole number, 1
-float === decimals, 4.5
-boolean === true / false
-
-
- x      =       "y"
-name assignment value
+# string === '', 'word', 1, 'functionName'
+# integer === whole number, 1
+# float === decimals, 4.5
+# boolean === true / false
 
 
-
-##lists
-
-# [] [value 1, value 2, etc]
-['word', 1, True]
-a list
-
-['word', ['word']] 
-nested list
-
-##dictionary
-    a seuquence of labled values
-
-{'wordName': 'wordValue'}
+#  x      =       "y"
+# name assignment value
 
 
+
+# ##lists
+
+# # [] [value 1, value 2, etc]
+# ['word', 1, True]
+# a list
+
+# ['word', ['word']] 
+# nested list
+
+# ##dictionary
+#     a seuquence of labled values
+
+# {'wordName': 'wordValue'}
+
+
+classSize = 10
+print("the amount of students in the class are", classSize)
