239a19a6ee8c0e049d132d4e0adfc57f6f5556ca
davidvaughan86 <david.vaughan86@gmail.com>
2021-03-10 11:13:10 -0500

    day 3

---
+++ week1/day3/notes.py
@@ -0,0 +1,16 @@
+variable
+
+# string === '', 'word', 1, 'functionName'
+# integer === whole number, 1
+# float === decimals, 4.5
+# boolean === true / false
+
+
+#  x      =       "y"
+# name assignment value
+
+sumofnumbers
+
+
+
+
