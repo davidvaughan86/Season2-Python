9eeba32da500e3cc242e01f7a96c9ff5cade3e3a
davidvaughan86 <david.vaughan86@gmail.com>
2021-03-10 11:14:36 -0500

    day 3 python

--- week1/day3/notes.py
+++ week1/day3/notes.py
@@ -1,16 +1,29 @@
-variable
+##variables
 
-# string === '', 'word', 1, 'functionName'
-# integer === whole number, 1
-# float === decimals, 4.5
-# boolean === true / false
+string === '', 'word', 1, 'functionName'
+integer === whole number, 1
+float === decimals, 4.5
+boolean === true / false
 
 
-#  x      =       "y"
-# name assignment value
+ x      =       "y"
+name assignment value
 
-sumofnumbers
 
+
+##lists
+
+# [] [value 1, value 2, etc]
+['word', 1, True]
+a list
+
+['word', ['word']] 
+nested list
+
+##dictionary
+    a seuquence of labled values
+
+{'wordName': 'wordValue'}
 
 
 
