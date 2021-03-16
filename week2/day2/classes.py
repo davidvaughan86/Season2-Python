496e7b3350f365ed97678a00ea017d64619442f6
davidvaughan86 <david.vaughan86@gmail.com>
2021-03-16 16:23:35 -0400

    classes and methods

---
+++ week2/day2/classes.py
@@ -0,0 +1,81 @@
+class Spider:
+    def __init__(self, name, strength, defense, hp):
+        self.name = name
+        self.strength = strength
+        self.defense = defense
+        self.hp = hp
+    pass
+
+spidey = Spider('spidey', 20, 10, 100)
+print(spidey.name)
+print(spidey.strength)
+venom = Spider('venom', 40, 50, 200)
+print(venom.name)
+print(venom.strength)
+carnage = Spider('carnage', 100, 100, 400)
+print(carnage.name)
+print(carnage.strength)
+
+petSpider = {
+    'name': 'spidey',
+    'strength': 20,
+    'defense': 10,
+    'hp':100
+}
+
+petSpider2 = {
+    'name': 'venom',
+    'strength': 40,
+    'defense': 50,
+    'hp':200
+}
+
+#feed the spider increase his strengthegnth
+def feedSpidey ():
+    petSpider['strength']+= 5
+
+# train the spider increase his defense
+def trainSpidey ():
+    petSpider['defense'] += 5
+
+
+
+def spideyStatus ():
+    print(petSpider)
+
+def stare ():
+    petSpider['hp'] += 5
+    
+   
+
+def welcomeMessage():
+    message = int(input('''
+    1. feed Spidey
+    2. train Spidey
+    3. Spidey status
+    4. stare
+    5. quit
+    '''))
+    
+    return message
+    
+
+choice = ''
+while choice != 5:
+    choice = welcomeMessage()
+    if choice == 1:
+        feedSpidey()
+        print(petSpider)
+    if choice == 2:
+        trainSpidey()
+        print(petSpider)
+    if choice == 3:
+        spideyStatus()
+        print(petSpider)
+    if choice == 4:
+        stare()
+        print(petSpider)
+    if choice == 5:
+        break
+    else:
+        pass
---
+++ week2/day2/methods.py
@@ -0,0 +1,89 @@
+class Spider:
+    def __init__(self, name, strength, defense, hp):
+        self.name = name
+        self.strength = strength
+        self.defense = defense
+        self.hp = hp
+        
+
+
+# functions defined inside of a class are called methods
+    def feed (self):
+        print('eating')
+        self.strength+= 5
+        
+
+    def train (self):
+        print('training')
+        self.defense += 5
+
+    def status (self):
+        print('how are you?')
+        print(petSpider)
+
+    def stare (self):
+        print('staring')
+        self.hp += 5
+
+
+# spidey = Spider('spidey', 20, 10, 100)
+# print(spidey.strength)
+# spidey.feed()
+# print(spidey.strength)
+
+def welcomeMessage():
+    message = int(input('''
+    1. feed 
+    2. train 
+    3. status
+    4. stare
+    5. quit
+    '''))
+    
+    return message
+    
+
+
+
+def playerSelect ():
+    print('please pick a character')
+    player = ''
+    
+    player1 = input('')
+    
+    
+    if player1 == 'spidey':
+        player = Spider('Spidey', 40, 25, 75)
+        print('youve chosen:'+ player.name)
+    if player1 == 'Venom':
+        player = Spider('Venom', 20, 50, 100)
+        print('youve chosen:'+ player.name)
+    if player1 == 'SpiderWoman':
+        player = Spider('Spiderwoman', 30, 100, 50)
+        print('youve chosen:'+ player.name)
+    
+    # print(player.name + 'is not named %s' %(player1))
+    return player
+       
+
+player = playerSelect()
+
+choice = ''
+while choice != 5:
+    choice = welcomeMessage()
+    if choice == 1:        
+        player.feed()
+        print(player.strength)
+    if choice == 2:
+        train()
+        print(petSpider)
+    if choice == 3:
+        status()
+        print(petSpider)
+    if choice == 4:
+        stare()
+        print(petSpider)
+    if choice == 5:
+        break
+    else:
+        pass
